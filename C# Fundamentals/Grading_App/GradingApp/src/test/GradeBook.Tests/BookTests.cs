bdaebffb9211c776b7935119a6b86caa70278957
Prajin Khadka <34759925+prajinkhadka@users.noreply.github.com>
2020-09-26 17:51:27 +0545

    C# Fundamentals.

---
+++ C# Fundamentals/Grading_App/GradingApp/src/test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var book = new Book("");
+
+        }
+    }
+}
